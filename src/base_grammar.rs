54342c17f314bddfa71acb5a77dcdb7bcd18b7b1
DaisyTian <jackietianwx@outlook.com>
2024-11-27 17:11:13 +0800

    closure_1

--- src/base_grammar.rs
+++ src/base_grammar.rs
@@ -1,9 +1,42 @@
 use core::{option::Option::Some, result::Result::Err};
-
+/* 基础语法 */
 fn base_grammar() {
+    let mut b = 10; // 可变变量 mutable，未指定类型
+
+    let a: u64 = 123; //如果没有声明类型，a 将自动被判断为有符号 32 位整型变量，这对于 a 的取值范围有很大的影响。
+
+    let y: f64 = 3.14; // y是不可变量 64位浮点数 指定类型
+
+    fn add(a: i32, b: i32) -> i32 {
+        // ->i32 代表返回值， 如果为空就是（）
+        a + b
+    }
+
+    let mut counter = 0; //无限循环，可以使用 break 退出循环。
+    loop {
+        counter += 1;
+        if counter == 10 {
+            break;
+        }
+    }
+
+    let mut number = 3;
+    while number != 0 {
+        println!("{}!", number); // 宏规则
+        number -= 1;
+    }
+
+    for number in 1..4 {
+        println!("{}!", number);
+    }
+
+    let squared_vec: Vec<i32> = vec.iter().map(|x| x * x).collect(); // vec.foreach(o=>o*o)
+
+    let filtered_vec: Vec<i32> = vec.into_iter().filter(|&x| x % 2 == 0).collect(); //vec.where(p=>p)
+                                                                                    /*  */
     //所有权借用
     let s = String::from("hello");
-    let len = calculate_length(&s);  // 借用
+    let len = calculate_length(&s); // 借用
     println!("The length of '{}' is {}.", s, len);
 
     // 枚举类型使用
@@ -11,13 +44,12 @@ fn base_grammar() {
     value_in_cents(coin);
 
     // 错误处理 Result
-    let a:i32 = 15;
-    let b:i32 = 10;
-    let result: Result<i32, String> = divide(a,b);
-    match result
-    {
-        Ok(value)=>println!("The divive of {}/{} is {}.", a,b,value),
-        Err(err_message)=>println!("Error: {}.",err_message),
+    let a: i32 = 15;
+    let b: i32 = 10;
+    let result: Result<i32, String> = divide(a, b);
+    match result {
+        Ok(value) => println!("The divive of {}/{} is {}.", a, b, value),
+        Err(err_message) => println!("Error: {}.", err_message),
     }
     // 错误处理 Option
     let mut v: Vec<i32> = Vec::new();
@@ -26,57 +58,54 @@ fn base_grammar() {
     v.push(3);
     v.push(4);
 
+    let idx: usize = 2;
+    let optionrs = get_element(idx, &v);
 
-    let idx:usize = 2;
-    let optionrs =  get_element(idx, &v);
-
-    match optionrs{
-        Some(value)=>println!("The {} value in vec is { }", idx,value),
-        None=>println!("No value"),
+    match optionrs {
+        Some(value) => println!("The {} value in vec is { }", idx, value),
+        None => println!("No value"),
     }
 
-    if let Some(value)=optionrs{
-        println!("The {} value in vec is { }", idx,value);
-    }else{
-        println!("No value"); 
+    if let Some(value) = optionrs {
+        println!("The {} value in vec is { }", idx, value);
+    } else {
+        println!("No value");
     }
 
-    let value = optionrs.map(|v| v * 2).unwrap_or(0); // 如果是 Some，计算双倍，如果是 None，返回 0  
-    println!("Processed Value: {}", value);  
+    let value = optionrs.map(|v| v * 2).unwrap_or(0); // 如果是 Some，计算双倍，如果是 None，返回 0
+    println!("Processed Value: {}", value);
 
     // 生命周期
     let s1 = String::from("Hello");
     let s2 = String::from("DaisyTian");
     let r1 = longesta(&s1, &s2);
     let r2 = longest(&s1, &s2);
-    println!("r1 is {},r2 is {}",r1,r2);
+    println!("r1 is {},r2 is {}", r1, r2);
 
     // 迭代器
-    let numbers = vec![1, 2, 3, 4, 5];  
-    
-    // 使用 fold 计算总和  
-    let sum = numbers.iter().fold(0, |acc, &x| acc + x);  
-    println!("Sum: {}", sum); // 输出: Sum: 15  
+    let numbers = vec![1, 2, 3, 4, 5];
+
+    // 使用 fold 计算总和
+    let sum = numbers.iter().fold(0, |acc, &x| acc + x);
+    println!("Sum: {}", sum); // 输出: Sum: 15
 
     // take 取值
-    let taken: Vec<_> = numbers.iter().take(3).collect();  
-    println!("Taken: {:?}", taken); // 输出: Taken: [1, 2, 3]  
+    let taken: Vec<_> = numbers.iter().take(3).collect();
+    println!("Taken: {:?}", taken); // 输出: Taken: [1, 2, 3]
 
     // enumerate 索引
-    for (index, value) in numbers.iter().enumerate(){  
-        println!("Index: {}, Value: {}", index, value);  
+    for (index, value) in numbers.iter().enumerate() {
+        println!("Index: {}, Value: {}", index, value);
     }
     //迭代器链
     let arr = [1, 2, 3, 4, 5];
     let mut iter = arr.into_iter().peekable();
-    while let Some(val) = iter.next() 
-    {
+    while let Some(val) = iter.next() {
         if val % 2 == 0 {
             continue;
         }
-    println!("{}", val);
+        println!("{}", val);
     }
-
 }
 
 fn calculate_length(s: &String) -> usize {
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,24 @@
-
 fn main() {
-   
+    // 闭包语法：let closure_name = |参数列表| 表达式或语句块;
+    let s = String::from("hello");
+    //按引用获取
+    let ref_use = || println!("ref to use:{}", s);
+    ref_use(); 
+    println!("参数 a 是{}", s);
+
+    //强制按值获取，所有权转移 
+    let take_s = move || println!("take ownship from {}", s);
+    take_s(); // 输出 "hello"
+    //println!("{}", s); // 这行代码将会报错，因为 s 的所有权已经被转移给了闭包
+
+    //Copy 类型（例如，基本整数/浮点数类型、布尔值、字符、固定大小数组和元组等），那么在闭包中使用它并不会导致所有权转移
+    let para_a: i32 = 5;
+    let take_use = move || println!("{}", para_a);
+    take_use();
+    println!("参数 a 是{}", para_a);//不报错，原因未知？
+
+    //可变借用捕获
+    let mut change_num = || num += 1;
+    change_num();
+    println!("num after closure = {}", num); // 输出: num after closure = 6
 }
